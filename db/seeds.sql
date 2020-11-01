780c3041dd9549db1a06c78cb6593e9e532f2ba0
Katie R-A <kreidanderson@msn.com>
2020-11-01 12:03:35 -0600

    almost everything working except for the submit button, but need to also work on the styling and add a burger photo

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (type) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (type) VALUES ('Blue Cheese Burger');
+INSERT INTO burgers (type, eaten) VALUES ('Big Mac', true);
+
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,55 @@
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+        var id = $(this).data("id");
+        var newBurger = $(this).data("newburger");
+    
+        var newBurgerState = {
+          eaten: newBurger
+        };
+    
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newBurgerState
+        }).then(
+          function() {
+            console.log("changed burger to", newBurger);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burger").val().trim(),
+        eaten: $("[name=eaten]:checked").val().trim()
+      };
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created a new burger");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger ", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+});
