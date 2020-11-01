983b508dfb226e87bb6dc5944e70f12d3b8b9f15
Katie R-A <kreidanderson@msn.com>
2020-11-01 16:37:57 -0600

    small changes

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,5 @@
 var connection = require("./connection.js");
 
-// function printQuestionMarks(num) {
-//   var arr = [];
-
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-
-//   return arr.toString();
-// }
-
 function objToSql(ob) {
   var arr = [];
 
