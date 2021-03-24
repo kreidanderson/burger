ee1d0e4d7d33a73d5a4d49bcd11b0d80de91bfc4
Katie R-A <kreidanderson@msn.com>
2021-03-24 17:57:58 -0400

    jawsdb change

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+if (process.env.JAWSDB_WHITE_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL)
 }else {
   var connection = mysql.createConnection({
     host: "localhost",
